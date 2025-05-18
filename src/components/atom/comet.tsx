4d24c76252a06ea58a7893b3976398cd03b313a3
Bagas Mahda Dhani <mahdabagas12@gmail.com>
2025-05-19 00:13:56 +0700

    feat: add meteor effect and icon

---
+++ src/components/atom/comet.tsx
@@ -0,0 +1,81 @@
+import { motion } from "framer-motion";
+import { useMemo } from "react";
+
+const generateComets = (count = 20) => {
+  const w = window.innerWidth;
+  const h = window.innerHeight;
+
+  return Array.from({ length: count }, (_, i) => {
+    const startX = Math.random() * (w + 300) - 100; // bisa sedikit
+    const startY = Math.random() * -300 - 100; // mulai di atas viewport
+    const angleOffset = w * 1.2; // sejauh 120% lebar layar
+    return {
+      id: i,
+      delay: 8 + Math.random() * 6, // 8–20 detik
+      duration: 8 + Math.random() * 10, // 8–12 detik
+      startX,
+      startY,
+      endX: startX - angleOffset, // geser ke kiri
+      endY: h + 300, // turun lewat bawah
+      size: 36 + Math.random() * 36, // ukuran bervariasi
+    };
+  });
+};
+
+export default function CometBackground() {
+  const comets = useMemo(() => generateComets(15), []); // 25 komet
+
+  return (
+    <div className="pointer-events-none fixed inset-0 overflow-hidden">
+      {comets.map((comet) => (
+        <motion.svg
+          key={comet.id}
+          viewBox="0 0 42 62"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute"
+          style={{
+            width: comet.size,
+            top: comet.startY,
+            left: comet.startX,
+          }}
+          initial={{ x: 0, y: 0, opacity: 1 }}
+          animate={{
+            x: comet.endX - comet.startX,
+            y: comet.endY - comet.startY,
+            opacity: 0,
+          }}
+          transition={{
+            duration: comet.duration,
+            delay: comet.delay,
+            repeat: Infinity,
+            repeatType: "loop",
+            ease: "linear",
+          }}
+        >
+          <path
+            d="M1.98486 60.3485L40.9849 0.348511"
+            stroke="url(#paint0_linear_51_10)"
+            strokeWidth="2"
+            strokeLinecap="round"
+          />
+
+          <defs>
+            <linearGradient
+              id="paint0_linear_51_10"
+              x1="40.6242"
+              y1="0.348512"
+              x2="21.7867"
+              y2="65.9325"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#999999" stopOpacity="0" />
+              <stop offset="0.711538" stopColor="white" stopOpacity="0.35" />
+              <stop offset="1" stopColor="white" stopOpacity="0.5" />
+            </linearGradient>
+          </defs>
+        </motion.svg>
+      ))}
+    </div>
+  );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,6 +7,15 @@ import City3 from "@/components/atom/city3";
 import Sun from "@/components/atom/sun";
 import Ground from "@/components/atom/ground";
 import { motion } from "motion/react";
+import CometBackground from "@/components/atom/comet";
+import {
+  Folder,
+  MessageCircle,
+  FileText,
+  Gamepad,
+  BookImage,
+  Code2,
+} from "lucide-react";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -17,7 +26,7 @@ function Index() {
 
   return (
     <div
-      className={`relative h-screen w-full overflow-hidden p-8 transition ${theme === "dark" ? "bg-night-5" : "bg-day-5"} `}
+      className={`relative h-screen w-full overflow-hidden transition ${theme === "dark" ? "bg-night-5" : "bg-day-5"} `}
     >
       <div className="fixed top-4 right-4 flex gap-2">
         <Switch
@@ -26,25 +35,61 @@ function Index() {
           onCheckedChange={(checked) => setTheme(checked ? "dark" : "light")}
         />
       </div>
+      <CometBackground />
       <motion.div
         animate={{ scale: 1 }}
         transition={{ duration: 2, delay: 8 }}
         initial={{ scale: 0 }}
-        className="absolute left-1/2 z-50 flex w-full -translate-x-1/2 flex-col items-center gap-4 pt-4"
+        className="absolute top-1/2 left-1/2 z-50 flex w-full -translate-x-1/2 -translate-y-2/3 flex-col gap-10 p-8 md:w-2/3 lg:w-1/2"
       >
-        <h4 className="font-lexend text-3xl text-white">21:45</h4>
-        <h1 className="font-tourney text-center text-5xl text-white">
-          Example_AI
-        </h1>
-        <div className="flex flex-col items-center gap-1">
-          <h4 className="font-lexend text-white">Malang Indonesia</h4>
-          <div className="w-40 border border-b-2" />
-          <h4 className="font-lexend text-white">May 25, 2002</h4>
+        <div className="flex flex-col items-center gap-4">
+          <h4 className="font-lexend text-3xl text-white">21:45</h4>
+          <h1 className="font-tourney text-center text-5xl text-white">
+            Example_AI
+          </h1>
+          <div className="flex flex-col items-center gap-1">
+            <h4 className="font-lexend text-white">Malang Indonesia</h4>
+            <div className="w-40 border border-b-2" />
+            <h4 className="font-lexend text-white">May 25, 2002</h4>
+          </div>
         </div>
-        <div className="grid grid-cols-3 gap-4">
-          <div className="bg-orange size-14 rounded-[10px] border border-white drop-shadow-md"></div>
-          <div className="bg-orange size-14 rounded-[10px] border border-white drop-shadow-md"></div>
-          <div className="bg-orange size-14 rounded-[10px] border border-white drop-shadow-md"></div>
+        <div className="grid grid-cols-4 justify-items-center gap-6">
+          <div className="flex flex-col items-center gap-1 text-white">
+            <div className="bg-orange flex size-16 items-center justify-center rounded-[10px] border border-white drop-shadow-md">
+              <Folder className="size-10 stroke-1 text-white" />
+            </div>
+            <p className="text-sm">Folder</p>
+          </div>
+          <div className="flex flex-col items-center gap-1 text-white">
+            <div className="bg-orange flex size-16 items-center justify-center rounded-[10px] border border-white drop-shadow-md">
+              <MessageCircle className="size-10 stroke-1 text-white" />
+            </div>
+            <p className="text-sm">Chat</p>
+          </div>
+          <div className="flex flex-col items-center gap-1 text-white">
+            <div className="bg-orange flex size-16 items-center justify-center rounded-[10px] border border-white drop-shadow-md">
+              <FileText className="size-10 stroke-1 text-white" />
+            </div>
+            <p className="text-sm">Document</p>
+          </div>
+          <div className="flex flex-col items-center gap-1 text-white">
+            <div className="bg-orange flex size-16 items-center justify-center rounded-[10px] border border-white drop-shadow-md">
+              <Gamepad className="size-10 stroke-1 text-white" />
+            </div>
+            <p className="text-sm">Game</p>
+          </div>
+          <div className="flex flex-col items-center gap-1 text-white">
+            <div className="bg-orange flex size-16 items-center justify-center rounded-[10px] border border-white drop-shadow-md">
+              <BookImage className="size-10 stroke-1 text-white" />
+            </div>
+            <p className="text-sm">Galery</p>
+          </div>
+          <div className="flex flex-col items-center gap-1 text-white">
+            <div className="bg-orange flex size-16 items-center justify-center rounded-[10px] border border-white drop-shadow-md">
+              <Code2 className="size-10 stroke-1 text-white" />
+            </div>
+            <p className="text-sm">Code</p>
+          </div>
         </div>
       </motion.div>
       <div className="absolute bottom-0 left-1/2 w-full -translate-x-1/2">
